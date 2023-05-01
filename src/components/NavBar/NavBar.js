b85131f50d0aaf0baf0f90bbb94d76eb7817b95a
juan martin mancuso <mancusojm@hotmail.com>
2023-05-02 00:24:36 +0200

    agrege logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,11 @@
 import CartWidget from "../CartWidget/CartWidget"
+import logo from './assets/logo.png'
 
 const NavBar = () => {
     return (
         <nav>
             <h3>Buenos Aires</h3>
+            <img src={logo} alt="logo"/>
             <div>
                 <button>Alfajores</button>
                 <button>Medialunas</button>
